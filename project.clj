dfb06233cf9da16fcc27e15fdde121b389a86c5c
Tim Gilbert <tim@kontor.com>
2018-02-08 11:13:25 -0500

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.workframe/stillsuit "0.1.0"
+(defproject com.workframe/stillsuit "0.2.0-SNAPSHOT"
   :description "lacinia-datomic utilities"
   :url "https://github.com/workframers/stillsuit"
   :pedantic? :warn
