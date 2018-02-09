24ad4d672e4ff88d86d09f374a419a6a5610f82e
Tim Gilbert <tim@kontor.com>
2018-02-09 16:03:42 -0500

    Fixed up dependency tree

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
 # Changelog
 
+## [0.2.0] - 2018-02-09
+### Fixed
+
+- Saner dependency tree (no log4j, datomic as `:scope "provided"` etc)
+
 ## [0.1.0] - 2018-02-08
 ### Added
 
 - Initial release
 - `lein release` task
 - Rudimentary docs
+
--- project.clj
+++ project.clj
@@ -15,12 +15,9 @@
                  [aero "1.1.2"]
                  [io.aviso/pretty "0.1.34"]
                  [com.walmartlabs/lacinia "0.24.0"]
-                 [com.datomic/datomic-pro "0.9.5656"]
+                 [com.datomic/datomic-pro "0.9.5656" :scope "provided"]
                  [clojure.java-time "0.3.1"]
                  [org.clojure/tools.logging "0.4.0"]
-                 [org.apache.logging.log4j/log4j-core "2.10.0"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.10.0"]
-
                  [org.clojure/tools.reader "1.2.1"]]
 
   :plugins [[s3-wagon-private "1.3.1" :exclusions [commons-logging]]]
@@ -42,21 +39,25 @@
                 :format  :html5
                 :to-dir  "target/manual"}
 
-  :profiles {:dev   {:plugins      [[lein-ancient "0.6.15"
-                                     :exclusions [commons-logging
-                                                  com.fasterxml.jackson.core/jackson-annotations
-                                                  com.fasterxml.jackson.core/jackson-core
-                                                  com.fasterxml.jackson.core/jackson-databind]]
-                                    [lein-cloverage "1.0.10"]
-                                    [lein-codox "0.10.3"]
-                                    [lein-shell "0.5.0"]
-                                    [lein-asciidoctor "0.1.14" :exclusions [org.slf4j/slf4j-api]]
-                                    [com.jakemccrary/lein-test-refresh "0.22.0"]]
-                     :dependencies [[vvvvalvalval/datomock "0.2.0"]
-                                    [codox-theme-rdash "0.1.2"]
-                                    [io.forward/yaml "1.0.6"]]}
-             :ultra {:plugins [[venantius/ultra "0.5.2" :exclusions [org.clojure/clojure]]]}
-             :test  {:resource-paths ["test/resources"]}}
+  :profiles {:dev     {:plugins      [[lein-cloverage "1.0.10"]
+                                      [lein-shell "0.5.0"]
+                                      [com.jakemccrary/lein-test-refresh "0.22.0"]]
+                       :dependencies [[vvvvalvalval/datomock "0.2.0"]
+                                      [io.forward/yaml "1.0.6"]
+                                      [org.apache.logging.log4j/log4j-core "2.10.0"]
+                                      [org.apache.logging.log4j/log4j-slf4j-impl "2.10.0"]
+                                      [com.datomic/datomic-pro "0.9.5656"
+                                       :exclusions [org.slf4j/slf4j-nop]]]}
+             :docs    {:plugins      [[lein-codox "0.10.3"]
+                                      [lein-asciidoctor "0.1.14" :exclusions [org.slf4j/slf4j-api]]]
+                       :dependencies [[codox-theme-rdash "0.1.2"]]}
+             :ancient {:plugins [[lein-ancient "0.6.15"
+                                  :exclusions [commons-logging
+                                               com.fasterxml.jackson.core/jackson-annotations
+                                               com.fasterxml.jackson.core/jackson-core
+                                               com.fasterxml.jackson.core/jackson-databind]]]}
+             :ultra   {:plugins [[venantius/ultra "0.5.2" :exclusions [org.clojure/clojure]]]}
+             :test    {:resource-paths ["test/resources"]}}
 
   :release-tasks [;; Make sure we're up to date
                   ["vcs" "assert-committed"]
